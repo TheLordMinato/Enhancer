339783931595cc7030c5698fbe71b7da754274fc
TheLordMinato <151508283+TheLordMinato@users.noreply.github.com>
2023-11-28 13:31:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,29 @@
 """ A Pyrogram Bot to Enhance image and Quality 🤖
     made by me 🗿"""
-
+import torch
 import asyncio
-from pyrogram import Client, filters, idle
-from pyrogram.enums import ChatAction
-from speedtest import Speedtest
-from PIL import Image as Img, ImageEnhance as Enhnc
+import requests
 from io import BytesIO as Io
+from speedtest import Speedtest
+from pyrogram import Client, filters
+from pyrogram.enums import ChatAction
 from config import API_ID, API_HASH, BOT_TOKEN
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image as Img, ImageEnhance as Enhnc, ImageFilter as Filter
 
+#Pyro Client 
 Bot = Client("Image Enhancer", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+#AI enhance model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = torch.hub.load('zhangqianhui/PyTorch-SR', 'esrgan', model_scale=4).to(device).eval()
+
+#start Handler
 @Bot.on_message(filters.private & filters.command("start"))
 async def start_command(_, msg):
     await msg.reply_text("hello i am image enhancer bot send any image")
 
+#image Handler, AI enhance code 
 @Bot.on_message(filters.private & filters.photo)
 async def enhance_image(bot, msg):    
     if msg.photo:
@@ -24,10 +33,21 @@ async def enhance_image(bot, msg):
         img = Img.open(path)
         edit = await text.edit("Enhancing your image, It can take some time")
         emoji = await msg.reply("⚡️")
+        img = img.resize((img.width * 2, img.height * 2), Img.ANTIALIAS)
+        tensor = ToTensor()(img).unsqueeze(0).to(device)
+        with torch.no_grad():
+            enhanced_tensor = model(img_tensor)
+        img = ToPILImage()(enhanced_tensor.squeeze(0).cpu())
+        img = img.filter(Filter.BLUR, )
+        color = Enhnc.Color(img)
+        img = color.enhance(1.1)
         enhancer = Enhnc.Contrast(img)
-        newimg = enhancer.enhance(1.1)
-        brightness = Enhnc.Brightness(newimg)
-        result = brightness.enhance(1.5)
+        img = enhancer.enhance(1.1)
+        brightness = Enhnc.Brightness(img)
+        img = brightness.enhance(1.1)
+        sharpness = Enhnc.Sharpness(img)
+        img = sharpness.enhance(1.1)
+        img = img.filter(ImageFilter.BLUR(radius=0.3))
         await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
         enhncd = Io()
         result.save(enhncd, format="JPEG", quality=600)
@@ -36,6 +56,7 @@ async def enhance_image(bot, msg):
         await edit.delete()
         await emoji.delete()
 
+#speed-test 
 @Bot.on_message(filters.command("testspeed"))
 async def run_speedtest(client, message):
     reply_msg = await message.reply_text("Analyzing...", quote=True)
@@ -64,4 +85,5 @@ def human_readable(size):
         var += 1
     return f"{round(size, 2)} {units[var]}"
 
-Bot.run()
+if __name__ == "__main__":
+    Bot.run()
